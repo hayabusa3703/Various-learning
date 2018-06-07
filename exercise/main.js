b9ef3d84bc1e31a88765193c3ec40ff617dc29db
hayabusa3703 <umibudou3703@gmail.com>
2018-06-07 11:03:14 +0900

    インクリメンタルサーチ機能の実装

--- exercise/main.js
+++ exercise/main.js
@@ -1,59 +1,38 @@
 var fruits = ['apple', 'apricot', 'avocado', 'blueberry', 'cherry', 'coconut', 'cranberry', 'dragonfruit', 'durian', 'grape', 'grapefruit', 'guava', 'kiwi fruit', 'lemon', 'lime', 'lychee', 'mango', 'melon', 'watermelon', 'miracle fruit', 'orange', 'bloodorange','clementine','mandarine','tangerine','papaya','passionfruit','peach','pear','persimmon','physalis','plum/prune','pineapple','pomegranate','raspberry','rambutan','star fruit','strawberry'];
 
+$(function() {
+  var list = $("#list");
+  var preWord;
 
+  function appendList(word) {
+    var item = $('<li class="list">').append(word);
+    list.append(item);
+  }
 
+  function editElement(element) {
+    var result = "^" + element;
+    return result;
+  }
 
-// $(function() {
-//   var list = $("#list");
-
-//   function appendList(word) {
-//     var item = $('<li class="list">').append(word);
-//     list.append(item);
-//   }
-
-//   $("#submit").on("click", function() {
-//     var input = $("#keyword").val();
-//     var reg = new RegExp("^" + input);
-
-//     $(".list").remove();
-
-//     $.each(fruits, function(i, fruit) {
-//       if (fruit.match(reg)) {
-//         appendList(fruit);
-//       }
-//     });
-
-//     if ($(".list").length === 0) {
-//       appendList("一致する果物はありませんでした");
-//     }
-//   });
-// });
-
-
-
-$(function(){
-	var list = $("#list");
-
-	function appendList(word){
-		var item = $('<li class= "list">').append(word);
-		list.append(item);
-	}
-
-
-	$("#submit").on("click", function(){
-		var input = $("#keyword").val();
-		var reg = new RegExp("^" + input);
-
-		$(".list").remove();
-
-		$.each(fruits, function(i, fruit) {
-			if (fruit.match(reg)) {
-				appendList(fruit);
-			}
-		});
-
-		if ($(".list").length === 0) {
-			appendList("一致するものないよ！！！！");
-		}
-	});
+  $("#keyword").on("keyup", function() {
+    var input = $("#keyword").val();
+    var inputs = input.split(" ").filter(function(e) { return e; });
+    var newInputs = inputs.map(editElement);
+    var word = newInputs.join("|");
+    var reg = RegExp(word);
+    if (word != preWord) {
+      $(".list").remove();
+      if(input.length !== 0) {
+        $.each(fruits, function(i, fruit) {
+          if (fruit.match(reg)) {
+            appendList(fruit);
+          }
+        });
+        if ($(".list").length === 0) {
+          appendList("一致する果物はありませんでした");
+        }
+      }
+    }
+    preWord = word;
+  });
 });
