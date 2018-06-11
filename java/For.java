5c5a1ab05838205f7d11c6135bb2b85559b38fdf
hayabusa3703 <umibudou3703@gmail.com>
2018-06-11 16:53:16 +0900

    For文の学習

---
+++ java/For.java
@@ -0,0 +1,7 @@
+class For {
+  public static void main(String[] args) {
+    String[] names = {"田中", "古村", "小松原"};
+    for(String name:names)
+    System.out.println("僕の名前は" + name + "です");
+  }
+}
