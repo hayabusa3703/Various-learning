cb4aa2e4a571cac81cb67029009029b15a32e981
hayabusa3703 <umibudou3703@gmail.com>
2018-06-14 17:37:01 +0900

    argument study1

---
+++ java/Argument1.java
@@ -0,0 +1,17 @@
+class Argument {
+  public static void main(String[] args) {
+
+    printData("カカロット");
+    
+
+    printData("地球生まれのサイヤ人孫悟空");
+    
+  }
+
+  // 引数を受け取るようにしてください
+  public static void printData(String name) {
+    // 「私の名前は◯◯です」と出力されるように書き換えてください
+    System.out.println("私の名前は" + name + "です");
+    
+  }
+}
